ef29b733f95da531b2b5388689905d1f0eeeacc7
Bart Durak <projektator@gmail.com>
2023-02-18 19:33:42 +0100

    Initial commit

---
+++ src/WhileUczen.java
@@ -0,0 +1,14 @@
+public class WhileUczen {
+    public static void main(String[] args) {
+        int uczniowie =1; int minut=0;
+        while (uczniowie <=10)
+        {
+            minut=minut + 4;
+            uczniowie= uczniowie *2;
+            System.out.println("minęło minut: " + minut);
+            System.out.println("zapytano uczniów: " +uczniowie);
+
+        }}
+
+    }
+
